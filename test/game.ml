967fb8a386b5e4eeb72c6240b433e992ef016baa
Tsukino Furukawa <tsukino95@gmail.com>
2019-07-01 14:20:08 +0900

    テスト用ページの追加

---
+++ test/game.ml
@@ -0,0 +1,48 @@
+open UniverseJs
+open World
+open Image
+open Color
+
+type world_t = {
+  mouse : string;
+  key : string;
+}
+
+let initial_world = {
+  mouse = "";
+  key = "";
+}
+
+let width = 400
+let height = 60
+
+let draw (world : world_t) : Image.t =
+  let mouse_text = text ("mouse: " ^ world.mouse) black in
+  let key_text = text ("key: " ^ world.key) black in
+  place_images
+    [mouse_text; key_text]
+    [(0., 0.); (0., 30.)]
+    (empty_scene (float_of_int width) (float_of_int height))
+
+let on_mouse world (x : float) (y : float) (command : string) : world_t =
+  {world with mouse =
+                "(" ^ (string_of_float x) ^ ", " ^ string_of_float y ^ ")  "
+                ^ command}
+let on_key_press world key_name : world_t =
+  {world with key = key_name ^ " pressed"}
+let on_key_release world key_name : world_t =
+  {world with key = key_name ^ " released"}
+
+(* ゲーム開始 *)
+let _ =
+  big_bang
+    initial_world
+    ~name:"test"
+    ~width:width
+    ~height:height
+    ~to_draw:draw
+    ~on_mouse:on_mouse
+    ~on_key_press:on_key_press
+    ~on_key_release:on_key_release
+    ~rate:1.			(* ゲームの動く速さ *)
+    
